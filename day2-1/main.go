d895d84e76390435895318ca9aa6a4836b1e0e48
Barnett Trzcinski <btrzcinski@gmail.com>
2017-01-13 18:05:33 -0800

    Finished day 2 part 1

---
+++ day2-1/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "strings"
+)
+
+func main() {
+    input_data, err := ioutil.ReadFile("input.txt")
+    if err != nil {
+        panic(err)
+    }
+
+    lines := strings.Split(strings.TrimSpace(string(input_data)), "\n")
+    for _, line := range lines {
+        x, y := 1, 1
+        moves := strings.Split(line, "")
+        for _, move := range moves {
+            switch move {
+            case "L":
+                if x > 0 {
+                    x -= 1
+                }
+            case "U":
+                if y > 0 {
+                    y -= 1
+                }
+            case "R":
+                if x < 2 {
+                    x += 1
+                }
+            case "D":
+                if y < 2 {
+                    y += 1
+                }
+            }
+        }
+        code := 1 + (3 * y) + x
+        fmt.Printf("%d", code)
+    }
+    fmt.Printf("\n")
+}
